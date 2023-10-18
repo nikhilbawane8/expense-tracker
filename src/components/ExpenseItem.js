6ba7a0b029977190d5f177614a651ef114cba0e2
nikhilbawane8 <nikhilbawane8@gmail.com>
2023-10-18 11:26:30 +0530

    goal

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,12 +7,12 @@ import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   return (
     <li>
-      <div className="container  ">
+      <div className="container p-2 ">
         <div className="expense-item">
           <ExpenseDate date={props.date} />
           <div className="   expense-item__description">
-            <h2 className="ms-2 badge border text-bg-primary">{props.title}</h2>
-            <div className="expense-item__price me-2  border btn btn-primary">
+            <h2 className="ms-3 badge border text-bg-primary">{props.title}</h2>
+            <div className="expense-item__price me-3  border btn btn-primary">
               ${props.amount}
             </div>
           </div>
